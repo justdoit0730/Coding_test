6af0a858d2ddaaf283ad5c7870989cb6fb9a35c2
junhee <a07308@naver.com>
2022-12-15 17:57:16 +0900

    프로그래머스 코딩테스트
    카펫
    
    22.12.15

---
+++ class/src/Programmers/lessons_42842.java
@@ -0,0 +1,28 @@
+package Programmers;
+
+import java.util.*;
+
+public class lessons_42842 { 
+	public static void main(String[] args) {
+		int brown = 24;
+		int yellow = 24;
+		
+		int []answer =new int [2];
+		int a=1;int b;
+
+		for(int i =a; i<=(int)Math.sqrt(yellow);i++) {
+			if(yellow%i==0) {
+				a=i;
+				b=yellow/i;
+				if(a+b==(brown-4)/2) {
+					answer[0]=b+2;
+					answer[1]=a+2;
+				}
+			}
+		}
+		
+		System.out.println(answer[0]);
+		System.out.println(answer[1]);
+		
+	}
+}
