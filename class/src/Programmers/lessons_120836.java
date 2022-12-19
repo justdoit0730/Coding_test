6ef04d8749336511d61b2fc65c971aa3e1b57e6a
junhee <a07308@naver.com>
2022-12-19 18:23:27 +0900

    프로그래머스 - 코딩테스트
    순서쌍의 개수
    
    22.12.19

---
+++ class/src/Programmers/lessons_120836.java
@@ -0,0 +1,16 @@
+package Programmers;
+
+import java.util.*;
+
+public class lessons_120836 { 
+	public static void main(String[] args) {
+		int n =20;
+		int answer =0;
+		for(int i=1; i<=n/2; i++) {
+			if(n%i==0) {
+				answer++;
+			}
+		}
+		System.out.println(answer+1);
+	}
+}
