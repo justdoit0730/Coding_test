b96575bc9fd25396fdd6424d7f857259c2bda4d9
junhee <a07308@naver.com>
2022-12-20 11:08:46 +0900

    프로그래머스 - 코딩테스트
    신고 결과 받기
    
    2022 KAKAO BLIND RECRUITMENT

---
+++ class/src/Programmers/lessons_92334.java
@@ -0,0 +1,38 @@
+package Programmers;
+
+import java.util.*;
+
+public class lessons_92334 { 
+	public static void main(String[] args) {
+		String[] id_list= {"muzi", "frodo", "apeach", "neo"};
+		String[] report= {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+		int k=2;
+		
+		int[] answer = new int[id_list.length];
+        Map<String, HashSet<String>> map = new HashMap<>(); 		//1
+        Map<String, Integer> idxMap = new HashMap<>();
+ 
+        for (int i = 0; i < id_list.length; i++) {			//2
+            String name = id_list[i];
+            map.put(name, new HashSet<>());
+            idxMap.put(name, i);
+        }
+ 
+        for (String s : report) {					//3
+            String[] str = s.split(" ");
+            String user = str[0];
+            String singo = str[1];
+            map.get(singo).add(user);
+        }	
+ 
+        for (int i = 0; i < id_list.length; i++) {			//4
+            HashSet<String> send = map.get(id_list[i]);
+            if (send.size() >= k) {
+                for (String name : send) {
+                    answer[idxMap.get(name)]++;
+                }
+            }
+        }
+		System.out.println(Arrays.toString(answer));
+	}
+}
